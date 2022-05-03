0fe8ea1ad188d16f86ce060d55f32526fb2f9169
Natalia Chunakova <n-chunakova@yandex.ru>
2022-05-03 17:57:16 +0400

    added simple MethodSource test

--- src/test/java/nchunakova/qa/tests/WebTest.java
+++ src/test/java/nchunakova/qa/tests/WebTest.java
@@ -2,23 +2,20 @@ package nchunakova.qa.tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import nchunakova.qa.domain.TestEnumItem;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
+import org.junit.jupiter.params.provider.*;
+import java.util.stream.Stream;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class WebTest {
 
+    @Disabled
     @ParameterizedTest(name = "Проверка поиска на сайте центробанка по слову {0}")
     @ValueSource(strings = {
             "ПНД",
@@ -30,9 +27,7 @@ public class WebTest {
 //        Шаги:
         $(".home-header_search_inp").setValue(testData).pressEnter();
 //        Ожидаемый результат:
-        $$(".subtitle")
-                .find(Condition.text(testData))
-                .shouldBe(Condition.visible);
+        $$(".subtitle").find(Condition.text(testData)).shouldBe(Condition.visible);
     }
 
     @Disabled
@@ -50,9 +45,7 @@ public class WebTest {
 //        Шаги:
         $(".home-header_search_inp").setValue(testData).pressEnter();
 //        Ожидаемый результат:
-        $$(".subtitle")
-                .find(Condition.text(expectedResult))
-                .shouldBe(Condition.visible);
+        $$(".subtitle").find(Condition.text(expectedResult)).shouldBe(Condition.visible);
     }
 
     @DisplayName("Смена фильтров для результата выдачи поиска через enum")
@@ -65,11 +58,31 @@ public class WebTest {
         $(".home-header_search_inp").setValue("ПНД").pressEnter();
         $(withText("За все время")).click();
 //        Ожидаемый результат:
-        $$(".filter-select_option")
-                .find(Condition.text(testData.searchFilter))
-                .click();
+        $$(".filter-select_option").find(Condition.text(testData.searchFilter)).click();
 
         //System.out.println(TestEnumItem.FIRSTBUTTON.searchFilter);
+    }
+
+
+
+    static Stream<Arguments> stringProvider() {
+        return Stream.of(
+                Arguments.of("амурский лесной", "Авторы Художник: А.С. Кунац. Скульптор: Е.И. Новикова." +
+                        " Чеканка: Санкт-Петербургский монетный двор (СПМД). Оформление гурта: 252 рифления."),
+                Arguments.of("кота в мешке", "Мисселинг, или Как не купить кота в мешке")
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Простой пример MethodSource: ищем по тексту {0} подзаголовок {1} в результате поисковой выдачи сайта")
+    @MethodSource("stringProvider")
+    void testWithMultiArgMethodSource(String first, String second) {
+        //        Предусловия:
+        Selenide.open("https://www.cbr.ru/");
+//        Шаги:
+        $(".home-header_search_inp").setValue(first).pressEnter();
+//        Ожидаемый результат:
+        $$(".subtitle").find(Condition.text(second)).shouldBe(Condition.visible);
     }
 
     @AfterEach
